8797de3a1a8cef6e244ed4aa0c5599536ee36bcc
Ridham Doshi <doshir@uwindsor.ca>
2021-03-01 19:08:50 -0500

    Name shuffle

---
+++ Random Name Generator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Random_Name_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+            List<string> nameList = new List<string> { "Ridham", "Meet", "Dhvanish", "Darshan" ,"Sachin","Parth"};
+            for (int i = 0; i < nameList.Count; i++)
+            {
+                string temp = nameList[i];
+                Random ran = new Random();
+                int randomIndex = ran.Next(i, nameList.Count);
+                nameList[i] = nameList[randomIndex];
+                nameList[randomIndex] = temp;
+            }
+            foreach (string a in nameList)
+                Console.WriteLine(a);
+
+        }
+    }
+}
